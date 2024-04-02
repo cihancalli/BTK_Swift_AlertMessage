e5d7748d1e362c9b55f4d5eded77b51b473c1b4d
Cihan Çallı <cihan.callii@gmail.com>
2024-04-02 10:21:34 +0300

    first commit

--- AlertMessage/ViewController.swift
+++ AlertMessage/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var labelSignUp: UILabel!
+    @IBOutlet weak var textFieldEmail: UITextField!
+    @IBOutlet weak var textFieldPassword: UITextField!
+    @IBOutlet weak var textFieldPasswordAgain: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func buttonSignUp(_ sender: Any) {
+        
+        if textFieldEmail.text == "" {
+            // email'ini girmemişsin
+            alertMessage(title: "Error Message",message: "Incorrect e-mail address entered")
+            
+        } else if textFieldPassword.text == "" {
+            //parolanı girmemişsin
+            alertMessage(title: "Error Message",message: "Incorrect password entered")
+        } else if textFieldPassword.text != textFieldPasswordAgain.text {
+            //parola uyuşmuyor
+            alertMessage(title: "Error Message",message: "Incorrect password again entered")
+        } else {
+            //kayıt işlemi başarılı
+            alertMessage(title: "Successful",message: "Registration has been completed")
+        }
+    }
+    
+    func alertMessage(title:String,message:String) {
+        let alertMessage = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) { UIAlertAction in
+            print("OK Button Entered")
+        }
+        
+        alertMessage.addAction(okButton)
+        self.present(alertMessage, animated: true, completion: nil)
+    }
 }
 
